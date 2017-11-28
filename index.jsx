abb5ac3e08303dd6493e392d0cbb168414126e90
Kira <kizxm@yahoo.com>
2017-11-28 11:32:06 -0800

    webpack building

--- index.jsx
+++
@@ -1,9 +0,0 @@
-reactDom.render(
-<div>
-<h1> Help Queue </h1>
-<h3> Here </h3>
-<h3> Kira </h3>
-<p><em> Will not save record </em></p>
-</div>,
-document.getElemeentById('react-app-root')
-);
---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function App(props){
+return(
+    <div>
+      <h1> Help Queue </h1>
+      <h3> Here </h3>
+      <h3> Kira </h3>
+      <p><em> Will not save record </em></p>
+      <hr />
+    </div>
+);
+
+}
+
+export default App;
